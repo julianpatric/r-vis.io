6239ea15849b247ca5b285f583c9638dd438ca64
julianpatric <julian.semilla@gmail.com>
2025-02-24 02:43:12 +0800

    content section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Topbar from './components/Topbar.jsx'
 import ServiceItem from './components/ServiceItem.jsx'
 import Footer from './components/Footer.jsx'
-import { SERVICES } from './data.js';
+import { CONTENT_ITEMS, SERVICES } from './data.js';
+
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <>
       <div className="header">
         <Topbar/>
-        <div className="hero-section"></div>
+        <div className="hero-section"> </div>
       </div>
       <div className="feature-section">
         <div className='container'>
@@ -23,10 +24,12 @@ function App() {
         </div>
       </div>
       <div className="content-section">
-        <div className='container'>
-          <div className="content-item"></div>
-          <div className="content-item"></div>
-          <div className="content-item"></div>
+        <div className='container content-grid'>
+          {CONTENT_ITEMS.map((content,index) => (
+            <div key={index} className={content.fullRow ? "content full-row" : "content two-column"}>
+              <img src={content.image} alt={content.title}></img>
+            </div>
+          ))}
         </div>
       </div>
       <Footer/>

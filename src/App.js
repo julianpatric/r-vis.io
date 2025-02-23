a16d721e21dc38252a64eefd0a2d55082c34e2d7
julianpatric <julian.semilla@gmail.com>
2025-02-23 23:37:16 +0800

    created footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Topbar from './components/Topbar.jsx'
 import ServiceItem from './components/ServiceItem.jsx'
+import Footer from './components/Footer.jsx'
 import { SERVICES } from './data.js';
 
 
@@ -9,9 +10,9 @@ function App() {
     <>
       <div className="header">
         <Topbar/>
-        <div className="section hero-section"></div>
+        <div className="hero-section"></div>
       </div>
-      <div className="section feature-section">
+      <div className="feature-section">
         <div className='container'>
           <h1>Architecture seen, felt, remembered.</h1>
           <ul className="services">
@@ -21,43 +22,15 @@ function App() {
           </ul>
         </div>
       </div>
-      <div className="section content-section">
+      <div className="content-section">
         <div className='container'>
           <div className="content-item"></div>
           <div className="content-item"></div>
           <div className="content-item"></div>
         </div>
       </div>
-      <div className="footer">
-        <div className="container">
-          <div className='footer-main'>
-          <div className="footer-email">
-            <p>Work with us:</p>
-            <a href="mailto:hello@r-vis.io">hello@r-vis.io</a>
-          </div>
-          <div className="footer-sitemap">
-            <ul>
-              <li><h4>r—vis</h4></li>
-              <li><a href="#">home</a></li>
-              <li><a href="#">about</a></li>
-              <li><a href="#">contact</a></li>
-            </ul>
-            <ul>
-              <li><h4>services</h4></li>
-              <li><a href="#">photo & film</a></li>
-              <li><a href="#">visualization</a></li>
-              <li><a href="#">graphic design</a></li>
-            </ul>
-            <ul>
-              <li><h4>socials</h4></li>
-              <li><a href="#">instagram</a></li>
-              <li><a href="#">facebook</a></li>
-              <li><a href="#">youtube</a></li>
-            </ul>
-          </div>
-        </div>
-        </div>
-      </div>
+      <Footer/>
+      
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <div className="footer-section">
+        <div className="container">
+          <div className='footer-main'>
+          <div className="footer-email">
+            <p>Work with us:</p>
+            <a href="mailto:hello@r-vis.io">hello@r-vis.io</a>
+          </div>
+          <div className="footer-sitemap">
+            <ul>
+              <li><h4>r—vis</h4></li>
+              <li><a href="#">home</a></li>
+              <li><a href="#">about</a></li>
+              <li><a href="#">contact</a></li>
+            </ul>
+            <ul>
+              <li><h4>services</h4></li>
+              <li><a href="#">photo & film</a></li>
+              <li><a href="#">visualization</a></li>
+              <li><a href="#">graphic design</a></li>
+            </ul>
+            <ul>
+              <li><h4>socials</h4></li>
+              <li><a href="#">instagram</a></li>
+              <li><a href="#">facebook</a></li>
+              <li><a href="#">youtube</a></li>
+            </ul>
+          </div>
+        </div>
+        </div>
+      </div>
+    )
+}

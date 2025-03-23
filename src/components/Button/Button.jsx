8644b84508b2c8b0a542b73245ffcb60124e461b
julianpatric <julian.semilla@gmail.com>
2025-03-24 03:05:48 +0800

    contact form

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,34 +1,27 @@
 import "./Button.css";
-import { useNavigate } from "react-router-dom";
-
 const Button = ({ children, to }) => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    if (to) {
-      navigate(to);
-    }
-  };
-
   return (
     <>
-      <div className="button" onClick={handleClick}>
-        {children}
-        <svg
-          className="arrow-icon"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <path
-            d="M5 12h14M12 5l7 7-7 7"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </div>
+      <a href={to}>
+        <div className="button">
+          {children}
+          <svg
+            className="arrow-icon"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M5 12h14M12 5l7 7-7 7"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </a>
     </>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,82 @@
 import "../styles/Contact.css";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
+import Button from "../components/Button/Button";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className="main-section">
         <div className="container about-container">
-          <div className="contact-left">
+          <div className="contact-left bg-red-500 text-white p-4">
             <h3>Get in touch</h3>
-            <h4 className="about-tip">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
-            </h4>
+            <p className="about-tip">
+              Interested in our services? Fill out this form and our team will
+              respond as soon as possible.
+            </p>
           </div>
-          <div className="contact-right"></div>
+          <div className="form-container">
+            <form className="contact-form">
+              <div class="form-item half">
+                <label for="first-name">First name*</label>
+                <input type="text" id="first-name" placeholder="John" />
+              </div>
+              <div class="form-item half">
+                <label for="last-name">Last name*</label>
+                <input type="text" id="last-name" placeholder="John" />
+              </div>
+
+              <div class="form-item">
+                <label for="email">Email*</label>
+                <input type="email" id="email" placeholder="you@company.com" />
+              </div>
+
+              <div class="form-item">
+                <label for="organization">Organization*</label>
+                <input
+                  type="text"
+                  id="organization"
+                  placeholder="Company, Inc."
+                />
+              </div>
+
+              <div class="form-item half">
+                <label for="interest">I'm interested in*</label>
+                <input type="text" id="interest" placeholder="Visualization" />
+              </div>
+              <div class="form-item half">
+                <label for="subject">Subject*</label>
+                <input type="text" id="subject" placeholder="Proposal" />
+              </div>
+
+              <div class="form-item">
+                <label for="message">Message*</label>
+                <textarea
+                  id="message"
+                  placeholder="My message is..."
+                ></textarea>
+              </div>
+
+              <Button type="submit" class="submit-button">
+                Send
+              </Button>
+            </form>
+          </div>
         </div>
       </div>
       <Footer />

8dc6efbb2e84f2f7092509c476e7dc273863b26e
julianpatric <julian.semilla@gmail.com>
2025-03-24 02:08:09 +0800

    contact page init, react-hook-form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,25 @@
-import CTA from "../components/CTA/CTA";
+import "../styles/Contact.css";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
 
 function Contact() {
   return (
-    <div className="main-section">
-      <div className="container">
-        <h3>Contact Page</h3>
+    <>
+      <Navbar />
+      <div className="main-section">
+        <div className="container about-container">
+          <div className="contact-left">
+            <h3>Get in touch</h3>
+            <h4 className="about-tip">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor
+            </h4>
+          </div>
+          <div className="contact-right"></div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
